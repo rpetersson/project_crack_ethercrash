c9457c203d6579573e3809a1ebc813396fa9eb5c
rpetersson <rpetersson@gmail.com>
2018-09-21 11:21:36 +0200

    MINOR FIXES

--- main.py
+++ main.py
@@ -8,52 +8,43 @@ db = sqlite3.connect(".\db.db")
 c = db.cursor()
 
 
-c.execute("SELECT * FROM tbl_multi ORDER BY ID ASC LIMIT 1000")
+c.execute("SELECT * FROM tbl_multi ORDER BY ID ASC LIMIT 5000")
 
 data = c.fetchall() #History of bets..
 c.close()
 
-bets = np.arange(1.00,10.00,0.01) # make bet array 1.01 -> 10x
+bets = np.arange(1.00,20.00,0.01) # make bet array 1.01 -> 10x
 
 print(bets)
 
 biggest_bank = {}
 
+# for bet in bets:
+
+bank = 10000
+bet_value = 10
+
+lostNr = 0
+
 for bet in bets:
 
-    bank = 10000
-    bet_value = 10
-
     for i in data:
-        if float(i[1]) >= round(bet,2):
+
+        #if float(i[1]) != 0:
+        bank = bank - bet_value
+
+        if float(i[1]) >= round(bet, 2):
+
             bank = bank + (bet_value * bet)
         else:
-            bank = bank - bet_value
+            lostNr = lostNr + 1
+            #print("Lost again! Bank = " + str(bank) + " lost times = " + str(lostNr))
             if bank <= 0:
                 break
-
-
 
     biggest_bank[round(bank, 1)]=round((bet),2) #Adds value of bank as index with bet as value
 
 
-
-# For plotting graphs...
-
-x = []
-y = []
-
-for item in biggest_bank:
-    y.append(item)
-    x.append(biggest_bank[item])
-
-
-num_bins = 5
-
-plt.show()
-print(biggest_bank)
-
 print(max(biggest_bank))
 
 print(biggest_bank[max(biggest_bank)])
-
