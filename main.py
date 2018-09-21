ecbfb5f197fbb37c444053f3a72f432b23f2b1d1
rpetersson <rpetersson@gmail.com>
2018-09-21 09:22:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import pandas as pd
+
+db = sqlite3.connect(".\db.db")
+c = db.cursor()
+
+
+c.execute("SELECT * FROM tbl_multi ORDER BY ID ASC LIMIT 500")
+
+data = c.fetchall()
+
+c.close()
+
+
+#make bet list 1.01 -> 10x
+
+bets = np.arange(1.00,10.00,0.01)
+
+#print(bets)
+
+
+biggest_bank = {}
+
+
+for bet in bets:
+
+    bank = 10000
+    bet_value = 10
+
+    for i in data:
+        if float(i[1]) >= float(bet):
+            bank = bank + (bet_value * bet)
+        else:
+            bank = bank - bet_value
+            if bank <= bet:
+                break
+
+
+    biggest_bank[round(bank, 1)]=round((bet),2)
+
+
+
+x = []
+y = []
+
+for item in biggest_bank:
+    y.append(item)
+    x.append(biggest_bank[item])
+plt.plot(x,y)
+plt.show()
+
+print(biggest_bank)
+
+print(max(biggest_bank))
+
+print(biggest_bank[max(biggest_bank)])
+
